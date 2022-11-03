bb7b29904dfdb3413c62e033d2b078e44fdd057e
yang9501 <yang9501@users.noreply.github.com>
2022-11-03 01:16:52 -0400

    Testing compile and docker push

--- surveySystem/src/main/java/com/surveySystem/SurveySystemApplication.java
+++ surveySystem/src/main/java/com/surveySystem/SurveySystemApplication.java
@@ -2,15 +2,9 @@ package com.surveySystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SurveySystemApplication extends SpringBootServletInitializer{
-	@Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(App.class);
-  }
+public class SurveySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurveySystemApplication.class, args);
